5679c194375ac72406b15a791ab0887e21b5f0c5
Gogy717 <1901862358@qq.com>
2024-09-20 22:24:06 +0800

    delete mulfunctioning server

--- 创新思维与实践/embedded/src/main.cpp
+++ 创新思维与实践/embedded/src/main.cpp
@@ -1,36 +1,31 @@
 #include <Arduino.h>
 #include "tcp_client.h"
+#include <Adafruit_NeoPixel.h>
 
 // put function declarations here:
-void TCPClientSetup();
+void SerialSetup();
+
+// put definitions here:
+#define RGB_PIN 38
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  TCPClientSetup();
-
+  SerialSetup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
+
+
 // put function definitions here:
-void TCPClientSetup() {
-  TCPClient client("127.0.0.1", 8080);
-
-  if (!client.connectToServer()) {
-    Serial.println("Connection to server failed");
-    return;
+void SerialSetup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
   }
-
-  if (!client.sendMessage("Hello from Arduino")) {
-    Serial.println("Sending message failed");
-    return;
-  }
-
-  std::string response = client.receiveMessage();
-  Serial.print("Server: ");
-  Serial.println(response.c_str());
+  Serial.println("Serial port is open");
 }
